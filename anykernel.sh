4eeeefba4b627d416d1e6515fc278b06a6d60e7a
Munir Nasibzade <euoryexe@gmail.com>
2025-01-31 18:08:08 +0400

    Change name of the kernel - just a rename

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Ryzen Kernel Build [Retrofit Dynamic] by mnasibzade @ telegram
+kernel.string=EklerKernel by mnasibzade @ telegram
 do.devicecheck=1
 do.modules=1
 do.systemless=0
